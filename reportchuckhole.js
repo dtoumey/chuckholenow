aed2736852b5e1783c7a387bca682ae40445fd65
Joel Klee <jklee@healthx.com>
2014-05-31 16:08:53 -0400

    pothole

--- reportchuckhole.js
+++ reportchuckhole.js
@@ -2,7 +2,16 @@ $(document).ready(function(){
 	$('#potholereporter').click(function() {
 		var myDataRef = new Firebase('https://incandescent-fire-4065.firebaseio.com/');	  
 		var username = 'hoosier driver';
-		var pothole_location =  {'Longitude': 39.93368,'Latitude': 89.55608}; 		 
+		var pothole_location =  getMyLocation();		 
 		myDataRef.push({username: username, pothole_location:pothole_location, status: 'open'});
 	});
 });
+
+
+function getMyLocation(){
+	var randomNumber = Math.floor(Math.random()*high_range) + low_range;
+	var longitude = 39 + Math.random.toFixed(5);
+	var latitude = 89 + Math.random().toFixed(5);
+	var pothole_location =  {'Longitude': longitude,'Latitude': latitude}; 	
+	return pothole_location;
+} 
